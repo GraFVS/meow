127c6ff31f1da906303e094149479d767d863299
GraFVS <GraF47232007>
2018-01-23 00:30:05 +0300

    Начало создания страниц

---
+++ src/main/java/pages/PassportYandexRu.java
@@ -0,0 +1,8 @@
+package pages;
+
+
+//url: https://passport.yandex.ru
+public class PassportYandexRu extends AnyPage {
+}
+
+
---
+++ src/main/java/steps/PassportYandexRuSteps.java
@@ -0,0 +1,4 @@
+package steps;
+
+public class PassportYandexRuSteps {
+}
